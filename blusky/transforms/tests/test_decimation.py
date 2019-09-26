ce5404caa1aa269e3b31bf2621d40cae1ad444b3
Ben Lasscock <blasscock@blasscock-mbp.local>
2019-09-26 09:42:54 -0500

    fix:tests updated to reflect new decimation method.

--- blusky/transforms/tests/test_decimation.py
+++ blusky/transforms/tests/test_decimation.py
@@ -26,15 +26,19 @@ class TestAlgorithms(unittest.TestCase):
 
     def test_default_decimation(self):
         """ test the default decimation method.
+
+            At the first layer we decimate according to scale. 
+            At subsequent layers, we decimate according to the difference 
+            in order with the current and previuous layers.
         """
         import numpy as np
 
         class test_wav(HasStrictTraits):
             scale = Int(1)
 
-        cascade_tree = CascadeTree(Input(shape=(99, 99, 1)), order=3)
+        cascade_tree = CascadeTree(Input(shape=(64,64, 1)), order=4)
         cascade_tree.generate(
-            [test_wav(scale=1), test_wav(scale=2), test_wav(scale=3)],
+            [test_wav(scale=1), test_wav(scale=2), test_wav(scale=3), test_wav(scale=4)],
             lambda x, y, z: x,
         )
 
@@ -45,7 +49,7 @@ class TestAlgorithms(unittest.TestCase):
         third_layer = second_layer.children[0]
 
         deci = DefaultDecimation(oversampling=0)
-        wav = np.empty((99, 99))
+        wav = np.empty((64, 64))
         wavp = deci.decimate_wavelet(wav, 2)
         np.array_equal(wav[::2, ::2], wavp)
 
@@ -57,9 +61,9 @@ class TestAlgorithms(unittest.TestCase):
         w, c = deci.resolve_scales(first_layer)
         self.assertTrue(w == 1 and c == 2)
         w, c = deci.resolve_scales(second_layer)
-        self.assertTrue(w == 2 and c == 4)
+        self.assertTrue(w == 2 and c == 2)
         w, c = deci.resolve_scales(third_layer)
-        self.assertTrue(w == 4 and c == 8)
+        self.assertTrue(w == 4 and c == 2)
 
         #
         deci = DefaultDecimation(oversampling=1)
@@ -71,7 +75,7 @@ class TestAlgorithms(unittest.TestCase):
         w, c = deci.resolve_scales(second_layer)
         self.assertTrue(w == 1 and c == 2)
         w, c = deci.resolve_scales(third_layer)
-        self.assertTrue(w == 2 and c == 4)
+        self.assertTrue(w == 2 and c == 2)
 
         #
         deci = DefaultDecimation(oversampling=2)
@@ -84,3 +88,43 @@ class TestAlgorithms(unittest.TestCase):
         self.assertTrue(w == 1 and c == 1)
         w, c = deci.resolve_scales(third_layer)
         self.assertTrue(w == 1 and c == 2)
+
+        deci = DefaultDecimation(oversampling=0)
+        
+        root_node = cascade_tree.root_node
+        first_layer = root_node.children[0].children[0].children[0]
+
+        # convolve with stride of 2 (scale 1 wavelet)
+        # Then convolve again, j2 - j1 is 1 so add a stride of 2, decimate wav by 2
+        # to adjust bandwidth to decimated signal
+        # Then convolve again  j3 - j2 is 1 so add a stride of 2, decimate wave by 4,
+        # we've decimated twice by 2 previously
+        w, c = deci.resolve_scales(first_layer)
+        self.assertTrue(w == 4 and c == 2)
+
+        deci = DefaultDecimation(oversampling=1)
+        # convolve with stride of 1 (scale 1 wavelet (oversampling))
+        # Then convolve again, j2 - j1 is 1 so add a stride of 2
+        # to adjust bandwidth to decimated signal
+        # Then convolve again  j3 - j2 is 1 so add a stride of 2, decimate wave by 2,
+        # we've decimated once by 2 previously
+        w, c = deci.resolve_scales(first_layer)
+        self.assertTrue(w == 2 and c == 2)
+        
+        deci = DefaultDecimation(oversampling=1)
+        first_layer = root_node.children[1].children[0].children[0]
+        
+        # convolve with stride of 2 (scale 2 wavelet (oversampling))
+        # Then convolve again, j2 - j1 is 1 so add a stride of 2
+        # to adjust bandwidth to decimated signal
+        # Then convolve again  j3 - j2 is 1 so add a stride of 2, decimate wave by 2,
+        # we've decimated twice by 2 previously
+        w, c = deci.resolve_scales(first_layer)
+        self.assertTrue(w == 4 and c == 2)
+        
+        deci = DefaultDecimation(oversampling=0)
+        first_layer = root_node.children[2].children[0]
+        # convolve with stride of 8 (scale 3 wavelet (oversampling))
+        # Then convolve again, j2 - j1 is 1 so add a stride of 2
+        w, c = deci.resolve_scales(first_layer)
+        self.assertTrue(w == 8 and c == 2)

8540545c017f756a9db7f3de17dad4fd4048dcfc
Ben Lasscock <blasscock@blasscock-mbp.local>
2019-11-04 13:38:42 -0600

    enh:adds 1-d wavelet codes.

---
+++ blusky/transforms/cascade_1d.py
@@ -0,0 +1,220 @@
+import re
+
+import keras.backend as keras_backend
+from keras.layers import DepthwiseConv2D, Lambda, Add
+import numpy as np
+
+from traits.api import Enum, HasStrictTraits, Int, Instance, List, Tuple
+
+from blusky.transforms.cascade_tree import CascadeTree
+from blusky.transforms.default_decimation import NoDecimation
+from blusky.transforms.i_decimation_method import IDecimationMethod
+from blusky.wavelets.i_wavelet_2d import IWavelet1D
+
+class Cascade1D(HasStrictTraits):
+    """
+    Caution this has a bug.
+
+    The idea here is to implement a cascade of convolvolution
+    and modulus opertations.
+    Suppose I had a sequence of wavelets, psi1, psi2, ...
+
+    |x * psi1|
+    |x * psi2| -> output
+        .
+        .
+        .
+     ---> ||x * psi1| * psi2|
+          ||x * psi1| * psi3|
+                .               -> output
+                .
+          ||x * psi2| * psi3|
+                .
+                .
+                  |
+                  |
+                  ---> .. etc ..
+    """
+
+    #: Provides methods for decimating at each layer in the transform.
+    decimation = Instance(IDecimationMethod, NoDecimation())
+
+    #: Subsequent convolutions can be applied to downsampled images for
+    #  efficiency.
+    # Provide some options with Keras for this:
+    # Max - MaxPooling (take max value in a window)
+    # Average - AveragePooling (average values in window)
+    pooling_type = Enum(["none", "max", "average"])
+
+    # shape of the input tile
+    shape = Tuple(Int)
+
+    #: In 2D we will apply the transform over a set of wavelets are different
+    # orientation, define that here in degrees.
+    angles = Tuple
+
+    #: Direction to Keras Conv2d on how to do padding at each convolution,
+    #  "same" pads with zeros, "valid" doesn't. This doesn't replace the
+    #  need to pad tiles during preprocessing, however, "same" will maintain
+    #  tile size through each layer.
+    #  "valid" is faster.
+    _padding = Enum(["same", "valid"])
+
+    #: private, labels endpoints to attach things to
+    _endpoint_counter = Int(0)
+
+    #:
+    _current_order = Int(1)
+
+    def _init_weights(
+        self, shape, node=None, dtype=None, wavelet1d=None, real_part=True
+    ):
+        """
+        Create an initializer for Conv1D layers. 
+
+        Parameters
+        ----------
+        wavelet1d - IWavelet2D
+            An object to create a wavelet.
+
+        dtype - Float
+            Data type for the wavelet, default is float32
+
+        real_part - Bool
+            If true it will initialize the convolutional weights
+            with the real-part of the wavelet, if false, the
+            imaginary part.
+
+        Returns
+        -------
+        returns - tensorflow variable
+            returns a tensorflow variable containing the weights.
+        """
+        if dtype is None:
+            dtype = np.float32
+
+        # precompute decimation
+        wavelet_stride, conv_stride = self.decimation.resolve_scales(node)
+
+        weights = np.zeros(shape, dtype=dtype)
+
+        wav = wavelet1d.kernel()
+
+        # keras does 32-bit real number convolutions
+        if real_part:
+            x = wav.real.astype(np.float32)
+        else:
+            x = wav.imag.astype(np.float32)
+
+        # apply to each input channel
+        for ichan in range(shape[2]):
+            weights[:, ichan, 0] = x[: shape[0]]
+
+        return keras_backend.variable(value=weights, dtype=dtype)
+
+    def _convolve_and_abs(self, wavelet, inp, node, trainable=False):
+        """
+        Implement the operations for |inp*psi| in 1-D. Assumes a single 
+        input channel. If you have multiple input channels you want 
+        the convolution to apply to, then to each independently.
+        
+        Parameters
+        ----------
+        wavelet - IWavelet1D
+            A wavelet object used to generate weights for each angles,
+            defined in self.angles.
+        inp - Keras Layer
+            A keras layer to apply the convolution to. For example,
+            an input layer. Or subsequently the output of the previous
+            convolutions.
+        stride - Int
+            Set a stride across the convolutions. This should be determined
+            by the scale of the transform.
+        node - Node
+            Node in the tree.
+
+        Returns
+        -------
+        returns - Keras Layer
+            The result of the convolution and abs function.        
+        """
+
+        # create a valid layer name
+        name = re.sub("[*,.|_]", "", node.name)
+
+        #
+        wavelet_stride, conv_stride = self.decimation.resolve_scales(node)
+
+        # after decimation
+        wavelet_shape = (
+            wavelet.shape[0] // wavelet_stride,
+        )
+
+        square = Lambda(lambda x: keras_backend.square(x), trainable=False)
+        add = Add(trainable=False)
+
+        # The output gets a special name, because it's here we attach
+        # things to. We name to the (endpoint)
+        sqrt = Lambda(
+            lambda x: keras_backend.sqrt(x), trainable=False, name=name
+        )
+        self._endpoint_counter += 1
+
+        real_part = Conv1D(1,
+            kernel_size=wavelet_shape,
+            data_format="channels_last",
+            padding=self._padding,
+            strides=conv_stride,
+            trainable=trainable,
+            kernel_initializer=lambda args: self._init_weights(
+                args, node=node, real_part=True, wavelet1d=wavelet
+            ),
+        )(inp)
+        real_part = square(real_part)
+
+        imag_part = Conv1D(1,
+            kernel_size=wavelet_shape,
+            data_format="channels_last",
+            padding=self._padding,
+            strides=conv_stride,
+            trainable=trainable,
+            kernel_initializer=lambda args: self._init_weights(
+                args, node=node, real_part=False, wavelet1d=wavelet
+            ),
+        )(inp)
+        imag_part = square(imag_part)
+
+        result = add([real_part, imag_part])
+        return sqrt(result)
+
+    def _convolve(self, inp, psi, node):
+        """
+        This computes |inp*psi|.
+        Which, for efficiency, (optionally) downsamples the output of the
+        convolution.
+        """
+        # apply the conv_abs layers
+        conv = self._convolve_and_abs(psi, inp, node)
+
+        return conv
+
+    def transform(self, cascade_tree, wavelets):
+        """
+        Apply abs/conv operations to arbitrary order.
+        Doesn't apply the DC term, just the subsequent layers.
+
+        Parameters
+        ----------
+        inp - Keras Layer
+            The input at the root of the cascade. Would generally
+            be a Keras Input Layer.
+
+        Returns
+        -------
+        returns - Keras Model
+            Returns a keras model applying the conv/abs operations
+            of the scattering transform to the input.
+        """
+        cascade_tree.generate(wavelets, self._convolve)
+
+        return cascade_tree.get_convolutions()
---
+++ blusky/transforms/tests/test_cascade_1d.py
@@ -0,0 +1,158 @@
+from os import path
+
+from keras.models import Model
+from keras.layers import Input
+import numpy as np
+from PIL import Image
+from scipy.signal import convolve2d
+import unittest
+
+from blusky.wavelets.wavelet_factories_1d import vanilla_gabor_1d, vanilla_morlet_1d
+from blusky.transform.cascade_1d import Cascade1D
+import blusky.datasets as datasets
+
+
+class TestAlgorithms(unittest.TestCase):
+    def setUp(self):
+        # define the test cascade. use
+        self.cascade = Cascade2D(
+            "none",
+            0,
+            angles=(0.0, 45, 90)
+        )
+
+    def test_cascade_2d_results(self):
+        """
+        Create a 2d cascade with three wavelets and order 3, and compare
+        results with manual convolution.
+        """
+        from blusky.transforms.cascade_tree import CascadeTree
+
+        wavelets = [vanilla_morlet_1d(0.001, j=i, crop=10.) for i in range(1,6)]
+
+        # get the test image
+        img_path = path.join(
+            path.dirname(datasets.__file__), "test_tiles/chaotic0.png"
+        )
+        data = np.array(Image.open(img_path)).astype(np.float32)
+        data /= np.max(data)
+
+        (nh, nw) = data.shape
+
+        # Keras needs the input images to look like a list
+        imgs = np.array([data])
+        imgs.shape = list(imgs.shape) + [1]
+
+        # define the input layer of the network to have the
+        # shape of the input images in the test suite
+        inp = Input(shape=(nh, nw, 1))
+
+        cascade_tree = CascadeTree(inp, order=3)
+        cascade_tree.generate([wav1, wav2, wav3], self.cascade._convolve)
+        my_transform = cascade_tree.get_convolutions()
+
+        # create the transform
+        model = Model(inputs=inp, outputs=my_transform)
+
+        # run the image through the transform cascade
+        result = model.predict(imgs)
+
+        # extract the kernels of each of the wavelets for manual convolution
+        # we'll test using three different angles that we used to create the
+        # transform above.
+        wav1_k = wav1.kernel(0.0)
+        wav2_k = wav2.kernel(45.0)
+        wav3_k = wav3.kernel(90.0)
+
+        # get numpy array of the test input image
+        image_index = 0
+        x = imgs[image_index, :, :, 0]
+
+        # manual convolution, |x * psi_1|
+        conv = np.abs(convolve2d(x, wav1_k, mode="same"))
+
+        # the endpoints are the different convolutions in the transform
+        # the first endpoint represents |x * psi_1|
+        endpoint = 0
+        # the psi_1 kernel is at 0 degrees.  This is the first angle
+        # which will have index 0
+        angle_index = 0
+
+        # get the corresponding convolution result in the cascade
+        cnn_conv = result[endpoint][image_index, :, :, angle_index]
+
+        np.testing.assert_almost_equal(
+            conv, cnn_conv, err_msg="Convolution does not match test values."
+        )
+
+        # test the second order convolution ||x * psi_1| * psi_2|
+
+        conv2 = np.abs(convolve2d(conv, wav2_k, mode="same"))
+        # there are three first order convolutions.  The first second
+        # order convolution will be the endpoint with index 3
+        endpoint = 3
+        # this wavelet is at 45deg, the second angle used to build
+        # the cascade.
+        angle_index = 1
+
+        cnn_conv2 = result[endpoint][image_index, :, :, angle_index]
+
+        np.testing.assert_almost_equal(
+            conv2, cnn_conv2, err_msg="Convolution does not match test values."
+        )
+
+        # test the third order convolution |||x * psi_1| * psi_2| * psi_3|
+        endpoint = 6
+        # The third order convolution is done with the 90 deg psi_3.  This is
+        # at angle_index 5
+        angle_index = 5
+        conv3 = np.abs(convolve2d(conv2, wav3_k, mode="same"))
+        cnn_conv3 = result[endpoint][image_index, :, :, angle_index]
+
+        np.testing.assert_almost_equal(
+            conv3, cnn_conv3, err_msg="Convolution does not match test values."
+        )
+
+    def test_cascade_2d_dirac(self):
+        """
+        Test that a Dirac function input returns the original wavelet.
+        """
+        from blusky.transforms.cascade_tree import CascadeTree
+
+        wav1 = Morlet2D(
+            sample_rate=0.004,
+            center_frequency=90.0,
+            bandwidth=(50.0, 25.0),
+            crop=3.5,
+            taper=False,
+        )
+        wav1_k = wav1.kernel(0.0)
+
+        # Form the Dirac function, just a unit impulse in the center of the
+        # image.
+        dirac = np.zeros((99, 99))
+        dirac[50, 50] = 1.0
+
+        imgs = np.array([dirac])
+        imgs.shape = list(imgs.shape) + [1]
+
+        # define the input layer of the network to have the
+        # shape of the input images in the test suite
+        inp = Input(shape=(99, 99, 1))
+
+        cascade_tree = CascadeTree(inp, order=3)
+        cascade_tree.generate([wav1], self.cascade._convolve)
+        my_transform = cascade_tree.get_convolutions()
+
+        model = Model(inputs=inp, outputs=my_transform)
+
+        result = model.predict(imgs)
+
+        cnn_conv_dirac = result[0, :, :, 0]
+        cnn_conv_dirac_crop = cnn_conv_dirac[44:57, 44:57]
+
+        np.testing.assert_almost_equal(
+            np.abs(wav1_k),
+            cnn_conv_dirac_crop,
+            err_msg="Convolution does not match test values.",
+        )

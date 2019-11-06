1176a689f489c42073cb4dfe1cf2ca16317504d6
Ben Lasscock <blasscock@blasscock-mbp.local>
2019-11-06 11:27:34 -0600

    test:adds test for father wavelet.

--- blusky/transforms/tests/test_cascade_1d.py
+++ blusky/transforms/tests/test_cascade_1d.py
@@ -7,6 +7,7 @@ from scipy.signal import convolve
 import unittest
 
 
+from blusky.transforms.apply_father_wavelet_1d import ApplyFatherWavlet1D
 import blusky.datasets as datasets
 from blusky.transforms.default_decimation import NoDecimation
 from blusky.transforms.cascade_1d import Cascade1D
@@ -40,7 +41,8 @@ class TestAlgorithms(unittest.TestCase):
         wavelets = [vanilla_morlet_1d(self.sample_rate, j=i)
                         for i in range(0, self.J)]
         father_wavelet = vanilla_gabor_1d(self.sample_rate, j=self.J)
-
+        father_wavelet = father_wavelet.kernel(shape=(self.N,))
+        
         # extract the kernels of each of the wavelets for manual convolution
         # we'll test using three different angles that we used to create the
         # transform above.
@@ -49,23 +51,34 @@ class TestAlgorithms(unittest.TestCase):
         wav3_k = wavelets[2].kernel()
 
         x = np.pad(self.ts[:,0], (128,128), mode="reflect")
-        print (x.shape)
         
         # manual convolution, |x * psi_1|
         self.conv1 = np.abs(convolve(x, wav1_k, mode="same"))
         self.conv2 = np.abs(convolve(self.conv1, wav2_k, mode="same"))        
         self.conv3 = np.abs(convolve(self.conv2, wav3_k, mode="same"))
 
+
+        # manual convolution, |x * psi_1| * \phi
+        self.sca1 = np.abs(convolve(self.conv1, father_wavelet, mode="valid"))
+        self.sca2 = np.abs(convolve(self.conv2, father_wavelet, mode="valid"))
+        self.sca3 = np.abs(convolve(self.conv3, father_wavelet, mode="valid"))
+
+        
         # unpad
         self.conv1 = self.conv1[128:-128]        
         self.conv2 = self.conv2[128:-128]
         self.conv3 = self.conv3[128:-128]
+        
+        # unpad
+        self.sca1 = self.sca1[128:-128]        
+        self.sca2 = self.sca2[128:-128]
+        self.sca3 = self.sca3[128:-128]
 
+        
     def test_cascade_1d_results(self):
         # vanilla filter bank
         wavelets = [vanilla_morlet_1d(self.sample_rate, j=i)
                         for i in range(0, self.J)]
-        father_wavelet = vanilla_gabor_1d(self.sample_rate, j=self.J)
 
         deci = NoDecimation()
         inp = Input(shape=(self.N,1))
@@ -119,3 +132,64 @@ class TestAlgorithms(unittest.TestCase):
         )
 
         
+    def test_apply_father_wavelet(self):
+        # vanilla filter bank
+        wavelets = [vanilla_morlet_1d(self.sample_rate, j=i)
+                        for i in range(0, self.J)]
+        father_wavelet = vanilla_gabor_1d(self.sample_rate, j=self.J)
+
+        deci = NoDecimation()
+        inp = Input(shape=(self.N,1))
+
+        # pad
+        pad_1d = Pad1D(wavelets, decimation=deci)
+        padded = pad_1d.pad(inp)
+
+        #
+        cascade_tree = CascadeTree(padded, order=self.order)
+        cascade = Cascade1D(decimation=deci)
+        convs = cascade.transform(cascade_tree, wavelets=wavelets)
+
+        # Create layers to remove padding
+        cascade_tree = CascadeTree(padded, order=self.order)
+        cascade_tree.generate(wavelets, pad_1d._unpad_same)
+        unpad = cascade_tree.get_convolutions()
+
+        # Remove the padding
+        unpadded_convs = [i[1](i[0]) for i in zip(convs, unpad)]
+
+        appl = ApplyFatherWavlet1D(wavelet=father_wavelet, 
+                        J=self.J, img_size=(self.N,),
+                        sample_rate=self.sample_rate)
+        
+        sca_transf = appl.convolve(unpadded_convs)
+        
+        model = Model(inputs=inp, outputs=sca_transf)
+        
+        result = model.predict(np.expand_dims(self.ts, axis=0))
+
+        cnn_result_1 = np.squeeze(result[0])
+        cnn_result_2 = np.squeeze(result[5])
+        cnn_result_3 = np.squeeze(result[-10])
+        
+        np.testing.assert_allclose(
+            self.sca1,
+            cnn_result_1,
+            atol=1E-3,
+            err_msg="first order does not match with cnn result.",
+        )
+
+        np.testing.assert_allclose(
+            self.sca2,
+            cnn_result_2,
+            atol=1E-3,
+            err_msg="first order does not match with cnn result.",
+        )
+
+        np.testing.assert_allclose(
+            self.sca3,
+            cnn_result_3,
+            atol=1E-3,
+            err_msg="first order does not match with cnn result.",
+        )
+
